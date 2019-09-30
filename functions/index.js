56907aba03836c002a24d57d3fa926a48659ccce
Pascal Achard <pascal.achard@gmail.com>
2019-09-30 16:59:02 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,7 @@
 require("dotenv").config()
 
+console.log('process.env.STRIPE_SECRET_KEY => ', process.env.STRIPE_SECRET_KEY);
+
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY),
   headers = {
     "Access-Control-Allow-Origin": "*",
